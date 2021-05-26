2347560931596cb98020c19b7a777b8c60f808c3
D.R.racer <drracer@drracer.eu>
2021-05-26 14:34:07 +0200

    Take over permanent storage implementation from MM-control-01

--- src/hal/eeprom.h
+++ src/hal/eeprom.h
@@ -1,12 +1,20 @@
 #pragma once
+#include <stdint.h>
 
 namespace hal {
 namespace EEPROM {
 
-/// EEPROM interface
-void WriteByte(uint16_t addr, uint8_t value);
-void UpdateByte(uint16_t addr, uint8_t value);
-uint8_t ReadByte(uint16_t addr);
+    /// EEPROM interface
+    void WriteByte(const uint8_t *addr, uint8_t value);
+    void UpdateByte(const uint8_t *addr, uint8_t value);
+    uint8_t ReadByte(const uint8_t *addr);
+
+    void WriteWord(const uint8_t *addr, uint16_t value);
+    void UpdateWord(const uint8_t *addr, uint16_t value);
+    uint16_t ReadWord(const uint8_t *addr);
+
+    /// @returns physical end address of EEPROM memory end
+    constexpr const uint16_t End();
 
 } // namespace EEPROM
 } // namespace hal
