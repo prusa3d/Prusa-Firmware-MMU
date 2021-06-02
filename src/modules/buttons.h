e2803c66ef8ce85238c34927f4389548f1a98417
D.R.racer <drracer@drracer.eu>
2021-06-02 13:07:28 +0200

    Add debouncing for FINDA

--- src/modules/buttons.h
+++ src/modules/buttons.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include "../hal/adc.h"
+#include "debouncer.h"
 
 /// Buttons are built on top of the raw ADC API
 /// This layer should contain debouncing of buttons and their logical interpretation
@@ -9,43 +10,13 @@
 namespace modules {
 namespace buttons {
 
-struct Button {
+struct Button : public debounce::Debouncer {
     inline constexpr Button()
-        : timeLastChange(0) {}
-
-    /// @returns true if button is currently considered as pressed
-    inline bool Pressed() const { return f.state == State::WaitForRelease; }
-
-    /// State machine stepping routine
-    void Step(uint16_t time, bool press);
+        : debounce::Debouncer(debounce) {}
 
 private:
     /// time interval for debouncing @@TODO specify units
     constexpr static const uint16_t debounce = 100;
-
-    /// States of the debouncing automaton
-    /// Intentionally not modeled as an enum class
-    /// as it would impose additional casts which do not play well with the struct Flags
-    /// and would make the code less readable
-    enum State { Waiting = 0,
-        Detected,
-        WaitForRelease,
-        Update };
-
-    /// The sole purpose of this data struct is to save RAM by compressing several flags into one byte on the AVR
-    struct Flags {
-        uint8_t state : 2; ///< state of the button
-        uint8_t tmp : 1; ///< temporary state of button before the debouncing state machine finishes
-        inline constexpr Flags()
-            : state(State::Waiting)
-            , tmp(false) {}
-    };
-
-    /// Flags and state of the debouncing automaton
-    Flags f;
-
-    /// Timestamp of the last change of ADC state for this button
-    uint16_t timeLastChange;
 };
 
 class Buttons {
@@ -70,6 +41,8 @@ private:
     /// @returns index of the button pressed or -1 in case no button is pressed
     static int8_t Sample(uint16_t rawADC);
 };
+
+extern Buttons buttons;
 
 } // namespace buttons
 } // namespace modules
--- src/modules/finda.cpp
+++ src/modules/finda.cpp
@@ -6,13 +6,8 @@ namespace finda {
 
 FINDA finda;
 
-uint8_t FINDA::Status() const {
-    // we can read ADC directly
-    return hal::adc::ReadADC(1) > 512;
-}
-
-void FINDA::Step() {
-    // in this implementation FINDA doesn't need any stepping
+void FINDA::Step(uint16_t time) {
+    debounce::Debouncer::Step(time, hal::adc::ReadADC(1) > adcDecisionLevel);
 }
 
 } // namespace finda
