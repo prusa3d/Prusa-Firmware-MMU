b13a38bbd4ce0c279071e0e20df8ea42afea5ba4
D.R.racer <drracer@drracer.eu>
2021-06-10 11:56:34 +0200

    Add empty EEPROM HAL implementation

---
+++ src/hal/avr/eeprom.cpp
@@ -0,0 +1,11 @@
+#include "../eeprom.h"
+
+namespace hal {
+namespace eeprom {
+
+uint8_t ReadByte(const uint8_t *addr) {
+    return 0;
+}
+
+} // namespace eeprom
+} // namespace hal
--- src/hal/eeprom.h
+++ src/hal/eeprom.h
@@ -2,19 +2,19 @@
 #include <stdint.h>
 
 namespace hal {
-namespace EEPROM {
+namespace eeprom {
 
-    /// EEPROM interface
-    void WriteByte(const uint8_t *addr, uint8_t value);
-    void UpdateByte(const uint8_t *addr, uint8_t value);
-    uint8_t ReadByte(const uint8_t *addr);
+/// EEPROM interface
+extern void WriteByte(const uint8_t *addr, uint8_t value);
+extern void UpdateByte(const uint8_t *addr, uint8_t value);
+extern uint8_t ReadByte(const uint8_t *addr);
 
-    void WriteWord(const uint8_t *addr, uint16_t value);
-    void UpdateWord(const uint8_t *addr, uint16_t value);
-    uint16_t ReadWord(const uint8_t *addr);
+extern void WriteWord(const uint8_t *addr, uint16_t value);
+extern void UpdateWord(const uint8_t *addr, uint16_t value);
+extern uint16_t ReadWord(const uint8_t *addr);
 
-    /// @returns physical end address of EEPROM memory end
-    constexpr const uint16_t End();
+/// @returns physical end address of EEPROM memory end
+extern constexpr const uint16_t End();
 
 } // namespace EEPROM
 } // namespace hal
