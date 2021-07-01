e2ec745286e3a4b09ee8af9d0256f518089bd2c2
vintagepc <53943260+vintagepc@users.noreply.github.com>
2021-07-01 00:47:40 -0400

    Simple config header with example (#39)
    Simple config header with example

---
+++ src/config/config.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdint.h>
+
+/// Wrangler for assorted compile-time configuration and constants.
+namespace config {
+
+/// Max number of extruders
+static constexpr uint8_t tool_count = 5U;
+
+} // namespace config
