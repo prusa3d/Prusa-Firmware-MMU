531f60ed278ea3b8398f1ccd56c427362cff7ef0
Yuri D'Elia <wavexx@thregr.org>
2021-07-04 15:41:42 +0200

    Add modules::speed_table for acceleration lookup tables

--- src/config/config.h
+++ src/config/config.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdint.h>
+#include "todo.h"
 
 /// Wrangler for assorted compile-time configuration and constants.
 namespace config {
---
+++ src/config/todo.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifndef __AVR__
+    #define F_CPU 16000000
+#endif
+
+#define MAX_STEP_FREQUENCY 40000 // Max step frequency
