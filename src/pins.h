1441ddb11bf1f9432e347c28ac9a10dc4f5d7bd5
Yuri D'Elia <wavexx@thregr.org>
2021-07-05 14:48:06 +0200

    GPIO: reformat affected files

--- src/pins.h
+++ src/pins.h
@@ -2,14 +2,14 @@
 #include "hal/gpio.h"
 
 /// pin definitions
-static constexpr hal::gpio::GPIO_pin TMC2130_SPI_MISO_PIN = {GPIOB, 3};
-static constexpr hal::gpio::GPIO_pin TMC2130_SPI_MOSI_PIN = {GPIOB, 2};
-static constexpr hal::gpio::GPIO_pin TMC2130_SPI_SCK_PIN  = {GPIOB, 1};
-static constexpr hal::gpio::GPIO_pin TMC2130_SPI_SS_PIN   = {GPIOB, 0};
+static constexpr hal::gpio::GPIO_pin TMC2130_SPI_MISO_PIN = { GPIOB, 3 };
+static constexpr hal::gpio::GPIO_pin TMC2130_SPI_MOSI_PIN = { GPIOB, 2 };
+static constexpr hal::gpio::GPIO_pin TMC2130_SPI_SCK_PIN = { GPIOB, 1 };
+static constexpr hal::gpio::GPIO_pin TMC2130_SPI_SS_PIN = { GPIOB, 0 };
 
-static constexpr hal::gpio::GPIO_pin SHR16_DATA  = {GPIOB, 5}; ///DS
-static constexpr hal::gpio::GPIO_pin SHR16_LATCH = {GPIOB, 6}; ///STCP
-static constexpr hal::gpio::GPIO_pin SHR16_CLOCK = {GPIOC, 7}; ///SHCP
+static constexpr hal::gpio::GPIO_pin SHR16_DATA = { GPIOB, 5 }; ///DS
+static constexpr hal::gpio::GPIO_pin SHR16_LATCH = { GPIOB, 6 }; ///STCP
+static constexpr hal::gpio::GPIO_pin SHR16_CLOCK = { GPIOC, 7 }; ///SHCP
 
-static constexpr hal::gpio::GPIO_pin USART_RX = {GPIOD, 2};
-static constexpr hal::gpio::GPIO_pin USART_TX = {GPIOD, 3};
+static constexpr hal::gpio::GPIO_pin USART_RX = { GPIOD, 2 };
+static constexpr hal::gpio::GPIO_pin USART_TX = { GPIOD, 3 };
