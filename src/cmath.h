fcb1b0c85df1a095fbdcf201f79e08ed433b5299
Yuri D'Elia <wavexx@thregr.org>
2021-07-13 01:08:33 +0200

    cmath.h: convert AVR min/max/abs into templates
    At least for min/max this will ensure types for both arguments are the
    same.
    
    This should be a little bit closer to the <cmath> definition that
    simply overloads these functions instead.

--- src/cmath.h
+++ src/cmath.h
@@ -3,15 +3,26 @@
 #pragma once
 
 #ifndef __AVR__
-    #include <cmath>
+#include <cmath>
 #else
 
-    // AVR libc doesn't support cmath
-    #include <math.h>
+// AVR libc doesn't support cmath
+#include <math.h>
 
-    // Use builtin functions for min/max/abs
-    #define min(a, b) __builtin_min((a, b))
-    #define max(a, b) __builtin_max((a, b))
-    #define abs(n) __builtin_abs((n))
+// Use builtin functions for min/max/abs
+template <typename T>
+static inline const T min(T a, T b) {
+    return __builtin_min((a, b));
+}
+
+template <typename T>
+static inline const T max(T a, T b) {
+    return __builtin_max((a, b));
+}
+
+template <typename T>
+static inline const T abs(T n) {
+    return __builtin_abs((n));
+}
 
 #endif
