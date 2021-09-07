0862964c82c0c398c9f6ed3e6f867b6325aa64c0
D.R.racer <drracer@drracer.eu>
2021-09-07 07:20:06 +0200

    Add cmake project for LUFA
    MMU-80

---
+++ lib/AddLufa.cmake
@@ -0,0 +1,11 @@
+add_library(
+  LUFA
+  lufa/LUFA/Common/Drivers/USB/Class/Device/CDCClassDevice.c
+  lufa/LUFA/Drivers/USB/Core/AVR8/Device_AVR8.c
+  lufa/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.c
+  lufa/LUFA/Drivers/USB/Core/AVR8/USBInterrupt_AVR8.c
+  lufa/LUFA/Drivers/USB/Core/ConfigDescriptors.c
+  lufa/LUFA/Drivers/USB/Core/Events.c
+  )
+
+# target_compile_features(LUFA PUBLIC cxx_std_14)
