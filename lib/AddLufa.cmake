b4ecf39ce5c8ac79cb34f85264832bb4bfd16fdc
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2021-09-20 11:31:51 +0200

    Fix LUFA build

--- lib/AddLufa.cmake
+++ lib/AddLufa.cmake
@@ -1,6 +1,6 @@
 add_library(
   LUFA
-  lufa/LUFA/Common/Drivers/USB/Class/Device/CDCClassDevice.c
+  lufa/LUFA/Drivers/USB/Class/Device/CDCClassDevice.c
   lufa/LUFA/Drivers/USB/Core/AVR8/Device_AVR8.c
   lufa/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.c
   lufa/LUFA/Drivers/USB/Core/AVR8/USBInterrupt_AVR8.c
