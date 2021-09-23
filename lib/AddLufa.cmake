57a3cfa109e48044361ff9fe5ed24d76521df0e2
D.R.racer <drracer@drracer.eu>
2021-09-23 13:43:18 +0200

    Fix formatting

--- lib/AddLufa.cmake
+++ lib/AddLufa.cmake
@@ -1,17 +1,17 @@
 add_library(
   LUFA
+  Descriptors.c
   lufa/LUFA/Drivers/USB/Class/Device/CDCClassDevice.c
   lufa/LUFA/Drivers/USB/Core/AVR8/Device_AVR8.c
+  lufa/LUFA/Drivers/USB/Core/AVR8/Endpoint_AVR8.c
+  lufa/LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
   lufa/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.c
   lufa/LUFA/Drivers/USB/Core/AVR8/USBInterrupt_AVR8.c
-  lufa/LUFA/Drivers/USB/Core/AVR8/Endpoint_AVR8.c
   lufa/LUFA/Drivers/USB/Core/ConfigDescriptors.c
-  lufa/LUFA/Drivers/USB/Core/Events.c
   lufa/LUFA/Drivers/USB/Core/DeviceStandardReq.c
+  lufa/LUFA/Drivers/USB/Core/Events.c
   lufa/LUFA/Drivers/USB/Core/USBTask.c
-  lufa/LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
-  Descriptors.c
-)
+  )
 
 target_include_directories(LUFA PRIVATE . lufa)
 target_compile_options(LUFA PRIVATE -include lufa_config.h)
