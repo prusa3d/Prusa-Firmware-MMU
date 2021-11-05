d22643ea8aabf53fc3c37d00b668c1ed87052c4b
Yuri D'Elia <wavexx@thregr.org>
2021-11-05 14:44:25 +0100

    AVR: Do not use __builtin_abs() for long types
    In AVR __builtin_abs() breaks for non-base types.
    
    Provide a generic function and use an overload when it is safe to use
    instead.
    
    This fixes the underlying step count calculation in PlanMove, thus
    removing the need for the PlanLongMove work-around.

--- src/cmath.h
+++ src/cmath.h
@@ -15,18 +15,22 @@ using std::min;
 #include <math.h>
 
 template <typename T>
-static inline const T min(T a, T b) {
+static inline const T min(const T a, const T b) {
     return a <= b ? a : b;
 }
 
 template <typename T>
-static inline const T max(T a, T b) {
+static inline const T max(const T a, const T b) {
     return a > b ? a : b;
 }
 
 template <typename T>
-static inline const T abs(T n) {
-    // Use builtin function when available
+static inline const T abs(const T n) {
+    return n < 0 ? -n : n;
+}
+
+static inline const int16_t abs(const int16_t n) {
+    // Use builtin function only when possible
     return __builtin_abs((n));
 }
 
