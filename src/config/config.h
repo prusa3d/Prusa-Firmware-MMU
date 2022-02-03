274385423acb81e90469bf97e1daea97b6a4c55d
D.R.racer <drracer@drracer.eu>
2022-02-03 16:39:03 +0100

    Enlarge load distance from fsensor to drive gear
    shall prevent printer load fails

--- src/config/config.h
+++ src/config/config.h
@@ -91,7 +91,7 @@ static constexpr U_mm feedToFinda = cuttingEdgeToFindaMidpoint + filamentMinLoad
 static constexpr U_mm maximumFeedToFinda = feedToFinda + 20.0_mm; ///< allow for some safety margin to load to FINDA
 static constexpr U_mm pulleyHelperMove = 10.0_mm; ///< Helper move for Load/Unload error states - when the MMU should slowly move the filament a bit
 static constexpr U_mm cutLength = 8.0_mm;
-static constexpr U_mm fsensorToNozzle = 20.0_mm; ///< ~20mm from MK4's filament sensor through extruder gears into nozzle
+static constexpr U_mm fsensorToNozzle = 30.0_mm; ///< ~20mm from MK4's filament sensor through extruder gears into nozzle
 static constexpr U_mm fsensorToNozzleAvoidGrind = 5.0_mm;
 /// Check the state of FSensor after this amount of filament got (hopefully) pulled out while unloading.
 static constexpr U_mm fsensorUnloadCheckDistance = 40.0_mm;
