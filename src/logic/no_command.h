c5aea89cac0261f0c56d1e64aac8fec79b666431
D.R.racer <drracer@drracer.eu>
2022-05-04 10:23:12 +0200

    Report FINDA/EEPROM disrepancy to the printer

--- src/logic/no_command.h
+++ src/logic/no_command.h
@@ -16,6 +16,11 @@ public:
 
     /// @returns true if the state machine finished its job, false otherwise
     bool StepInner() override { return true; }
+
+    /// Used to report initialization errors (which can be reported if the UART started up).
+    /// Intentionally only available in the "noCommand" operation
+    /// which is only active when the MMU starts and before it gets any other command from the printer.
+    inline void SetInitError(ErrorCode ec) { error = ec; }
 };
 
 /// The one and only instance of NoCommand state machine in the FW
