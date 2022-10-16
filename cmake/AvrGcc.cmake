2a8ba74737f65616a826b52d9c103166e2f9a6cd
Yuri D'Elia <wavexx@thregr.org>
2022-10-16 14:33:14 +0200

    cmake: Uniform Avr toolchain build with MK3
    - Also set AR/RANLIB in the toolchain
    - Fetch GCC version from bootstrap

--- cmake/AvrGcc.cmake
+++ cmake/AvrGcc.cmake
@@ -1,6 +1,4 @@
 get_filename_component(PROJECT_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
-
-set(AVR_GCC_VERSION 7.3.0)
-set(AVR_TOOLCHAIN_DIR "${PROJECT_CMAKE_DIR}/../.dependencies/avr-gcc-${AVR_GCC_VERSION}/")
-
+include("${PROJECT_CMAKE_DIR}/Utilities.cmake")
+get_dependency_directory("avr-gcc" AVR_TOOLCHAIN_DIR)
 include("${PROJECT_CMAKE_DIR}/AnyAvrGcc.cmake")
