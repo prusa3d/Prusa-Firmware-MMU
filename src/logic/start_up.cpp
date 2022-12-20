074f89c7514ac66b7526704761c92b0df83d6a0c
D.R.racer <drracer@drracer.eu>
2022-12-20 16:33:10 +0100

    Refactor CheckFINDAvsEEPROM and setup2

--- src/logic/start_up.cpp
+++ src/logic/start_up.cpp
@@ -2,11 +2,20 @@
 #include "start_up.h"
 #include "../modules/buttons.h"
 #include "../modules/finda.h"
+#include "../modules/globals.h"
 #include "../modules/user_input.h"
 
 namespace logic {
 
 StartUp startUp;
+
+bool StartUp::Reset(uint8_t) {
+    if (!CheckFINDAvsEEPROM()) {
+        SetInitError(ErrorCode::FINDA_VS_EEPROM_DISREPANCY);
+    }
+
+    return true;
+}
 
 bool StartUp::StepInner() {
     switch (state) {
@@ -20,7 +29,7 @@ bool StartUp::StepInner() {
             switch (error) {
             case ErrorCode::FINDA_VS_EEPROM_DISREPANCY:
                 // Retry
-                if (!mf::finda.CheckFINDAvsEEPROM()) {
+                if (!CheckFINDAvsEEPROM()) {
                     error = ErrorCode::FINDA_VS_EEPROM_DISREPANCY;
                     state = ProgressCode::ERRWaitingForUser;
                 } else {
@@ -43,4 +52,25 @@ bool StartUp::StepInner() {
     }
     return false;
 }
+
+bool StartUp::CheckFINDAvsEEPROM() {
+    bool ret = true;
+    if (mf::finda.Pressed() && mg::globals.FilamentLoaded() < mg::InFSensor) {
+        // This is a tricky situation - EEPROM doesn't have a record about loaded filament (blocking the Selector)
+        // To be on the safe side, we have to override the EEPROM information about filament position - at least InFSensor
+        // Moreover - we need to override the active slot position as well, because we cannot know where the Selector is.
+        // For this we speculatively set the active slot to 2 (in the middle ;) )
+        // Ideally this should be signalled as an error state and displayed on the printer and recovered properly.
+        //mg::globals.SetFilamentLoaded(2, mg::InFSensor);
+        ret = false;
+    } else if (!mf::finda.Pressed() && mg::globals.FilamentLoaded() >= mg::InSelector) {
+        // Opposite situation - not so dangerous, but definitely confusing to users.
+        // FINDA is not pressed but we have a record in the EEPROM.
+        // It has been decided, that we shall clear such a record from EEPROM automagically
+        // and presume there is no filament at all (requires working FINDA)
+        mg::globals.SetFilamentLoaded(config::toolCount, mg::AtPulley);
+    }
+    return ret;
+}
+
 } // namespace logic
