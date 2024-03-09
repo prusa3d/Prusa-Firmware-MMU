956b335b44bad2c8c7232c5fe6e7e120c877354c
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-03-09 13:06:05 +0100

    Make the size of the code properly include the version information

--- src/version.cpp
+++ src/version.cpp
@@ -1,11 +1,13 @@
 #include <version.hpp>
 
+
+// Define structure at the end of flash which contains information about the version of the firmware
 struct Signatures {
     uint8_t project_major;
     uint8_t project_minor;
     uint16_t project_revision;
     uint16_t project_build_number;
-} const signatures __attribute__((section(".user_signatures"), used)) = {
+} static constexpr signatures __attribute__((section(".version"), used)) = {
     PROJECT_VERSION_MAJOR,
     PROJECT_VERSION_MINOR,
     PROJECT_VERSION_REV,
